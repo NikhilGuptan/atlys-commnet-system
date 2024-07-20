b9c61cd251822796d405242c960817e85e57f2d3
Nikhil Gupta <nikhil.gupta@Savvytechies.com>
2024-07-20 17:20:29 +0530

    added route as well

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import React from 'react';
 import './App.css';
 import Login from './components/login';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Login/>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="*" element={<h1 className='default-route'>Route not found</h1>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
