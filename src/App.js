cf4564d8f98bd0fa66855b1a8aaf871b93481d50
Nikhil Gupta <nikhil.gupta@Savvytechies.com>
2024-07-20 17:24:14 +0530

    created post screen

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Login from './components/login';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import CommentPostScreen from './components/commentPostScreen';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<Login />} />
+          <Route path="/post" element={<CommentPostScreen />} />
           <Route path="*" element={<h1 className='default-route'>Route not found</h1>} />
         </Routes>
       </div>
---
+++ src/components/commentPostScreen.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import "./commentPostScreen.css";
+
+
+function CommentPostScreen() {
+
+    return (
+       <h1>hello</h1> 
+    );
+}
+
+export default CommentPostScreen;
